1ab442281375b432cabfc225e1e78e2cb8c3d9e8
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2026-03-29 18:02:13 -0400

    Strip zROS topic list to optical flow app essentials
    Remove all unused topic declarations, definitions, and shell echo
    handlers. Only retain the 6 topics needed by the optical flow app:
    argus, imu0, optical_flow_raw, status, vehicle_optical_flow,
    vehicle_optical_flow_vel.
    
    Saves ~17KB RAM and ~18KB flash from unused topic data buffers.
    
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/synapse/topic/src/synapse_topic_list.c
+++ drivers/synapse/topic/src/synapse_topic_list.c
@@ -15,8 +15,6 @@
 //*******************************************************************
 // helper functions
 //*******************************************************************
-static const char *unhandled = "UNHANDLED";
-
 void stamp_msg(synapse_pb_Timestamp *msg, int64_t ticks)
 {
 	int64_t sec = ticks / CONFIG_SYS_CLOCK_TICKS_PER_SEC;
@@ -26,235 +24,23 @@ void stamp_msg(synapse_pb_Timestamp *msg, int64_t ticks)
 	msg->nanos = nanosec;
 }
 
-const char *input_source_str(synapse_pb_Status_InputSource src)
-{
-	if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_UNKNOWN) {
-		return "unknown";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_RADIO_CONTROL) {
-		return "radio_control";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_ETHERNET) {
-		return "ethernet";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_CAN) {
-		return "can";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_RESERVED_0) {
-		return "reserved 0";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_RESERVED_1) {
-		return "reserved 1";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_RESERVED_2) {
-		return "reserved 2";
-	} else if (src == synapse_pb_Status_InputSource_INPUT_SOURCE_RESERVED_3) {
-		return "reserved 3";
-	}
-	return unhandled;
-}
-
-const char *topic_source_str(synapse_pb_Status_TopicSource src)
-{
-	if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_UNKNOWN) {
-		return "unknown";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_INPUT) {
-		return "input";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_LOCAL) {
-		return "local";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_ETHERNET) {
-		return "ethernet";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_CAN) {
-		return "can";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_RESERVED_0) {
-		return "reserved 0";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_RESERVED_1) {
-		return "reserved 1";
-	} else if (src == synapse_pb_Status_TopicSource_TOPIC_SOURCE_RESERVED_2) {
-		return "reserved 2";
-	}
-	return unhandled;
-}
-
-const char *mode_str(synapse_pb_Status_Mode mode)
-{
-	if (mode == synapse_pb_Status_Mode_MODE_UNKNOWN) {
-		return "unknown";
-	} else if (mode == synapse_pb_Status_Mode_MODE_CALIBRATION) {
-		return "calibration";
-	} else if (mode == synapse_pb_Status_Mode_MODE_ACTUATORS) {
-		return "manual";
-	} else if (mode == synapse_pb_Status_Mode_MODE_ATTITUDE_RATE) {
-		return "attitude rate";
-	} else if (mode == synapse_pb_Status_Mode_MODE_ATTITUDE) {
-		return "attitude";
-	} else if (mode == synapse_pb_Status_Mode_MODE_ALTITUDE) {
-		return "altitude";
-	} else if (mode == synapse_pb_Status_Mode_MODE_POSITION) {
-		return "position";
-	} else if (mode == synapse_pb_Status_Mode_MODE_VELOCITY) {
-		return "velocity";
-	} else if (mode == synapse_pb_Status_Mode_MODE_ACCELERATION) {
-		return "acceleration";
-	} else if (mode == synapse_pb_Status_Mode_MODE_BEZIER) {
-		return "bezier";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_0) {
-		return "reserved 0";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_1) {
-		return "reserved 1";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_2) {
-		return "reserved 2";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_3) {
-		return "reserved 3";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_4) {
-		return "reserved 4";
-	} else if (mode == synapse_pb_Status_Mode_MODE_RESERVED_5) {
-		return "reserved 5";
-	}
-	return unhandled;
-}
-
-const char *armed_str(synapse_pb_Status_Arming arming)
-{
-	if (arming == synapse_pb_Status_Arming_ARMING_UNKNOWN) {
-		return "unknown";
-	} else if (arming == synapse_pb_Status_Arming_ARMING_ARMED) {
-		return "armed";
-	} else if (arming == synapse_pb_Status_Arming_ARMING_DISARMED) {
-		return "disarmed";
-	}
-	return unhandled;
-}
-
-const char *safety_str(synapse_pb_Safety_Status safety)
-{
-	if (safety == synapse_pb_Safety_Status_SAFETY_UNKNOWN) {
-		return "unknown";
-	} else if (safety == synapse_pb_Safety_Status_SAFETY_SAFE) {
-		return "safe";
-	} else if (safety == synapse_pb_Safety_Status_SAFETY_UNSAFE) {
-		return "unsafe";
-	}
-	return unhandled;
-}
-
-const char *status_safety_str(synapse_pb_Status_Safety safety)
-{
-	if (safety == synapse_pb_Status_Safety_SAFETY_UNKNOWN) {
-		return "unknown";
-	} else if (safety == synapse_pb_Status_Safety_SAFETY_SAFE) {
-		return "safe";
-	} else if (safety == synapse_pb_Status_Safety_SAFETY_UNSAFE) {
-		return "unsafe";
-	}
-	return unhandled;
-}
-
-const char *link_status_str(synapse_pb_Status_LinkStatus status)
-{
-	if (status == synapse_pb_Status_LinkStatus_STATUS_UNKNOWN) {
-		return "unknown";
-	} else if (status == synapse_pb_Status_LinkStatus_STATUS_NOMINAL) {
-		return "nominal";
-	} else if (status == synapse_pb_Status_LinkStatus_STATUS_DISABLED) {
-		return "disabled";
-	} else if (status == synapse_pb_Status_LinkStatus_STATUS_LOSS) {
-		return "loss";
-	}
-	return unhandled;
-}
-
-const char *fuel_str(synapse_pb_Status_Fuel fuel)
-{
-	if (fuel == synapse_pb_Status_Fuel_FUEL_UNKNOWN) {
-		return "unknown";
-	} else if (fuel == synapse_pb_Status_Fuel_FUEL_CRITICAL) {
-		return "critical";
-	} else if (fuel == synapse_pb_Status_Fuel_FUEL_LOW) {
-		return "low";
-	} else if (fuel == synapse_pb_Status_Fuel_FUEL_NOMINAL) {
-		return "nominal";
-	} else if (fuel == synapse_pb_Status_Fuel_FUEL_OVER_CAPACITY) {
-		return "over capacity";
-	}
-	return unhandled;
-}
-
 /********************************************************************
  * topics
  ********************************************************************/
-ZROS_TOPIC_DEFINE(accel_ff, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(accel_sp, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(actuators, synapse_pb_Actuators);
-ZROS_TOPIC_DEFINE(altimeter, synapse_pb_Altimeter);
-ZROS_TOPIC_DEFINE(angular_velocity_ff, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(angular_velocity_sp, synapse_pb_Vector3);
 ZROS_TOPIC_DEFINE(argus, synapse_pb_ArgusResults);
-ZROS_TOPIC_DEFINE(attitude_sp, synapse_pb_Quaternion);
-ZROS_TOPIC_DEFINE(battery_state, synapse_pb_BatteryState);
-ZROS_TOPIC_DEFINE(bezier_trajectory, synapse_pb_BezierTrajectory);
-ZROS_TOPIC_DEFINE(bezier_trajectory_ethernet, synapse_pb_BezierTrajectory);
-ZROS_TOPIC_DEFINE(clock_offset_ethernet, synapse_pb_ClockOffset);
-ZROS_TOPIC_DEFINE(cmd_vel, synapse_pb_Twist);
-ZROS_TOPIC_DEFINE(cmd_vel_ethernet, synapse_pb_Twist);
-ZROS_TOPIC_DEFINE(force_sp, synapse_pb_Vector3);
 ZROS_TOPIC_DEFINE(imu0, synapse_pb_Imu);
-ZROS_TOPIC_DEFINE(imu1, synapse_pb_Imu);
-ZROS_TOPIC_DEFINE(imu2, synapse_pb_Imu);
-ZROS_TOPIC_DEFINE(imu_q31_array, synapse_pb_ImuQ31Array);
-ZROS_TOPIC_DEFINE(input, synapse_pb_Input);
-ZROS_TOPIC_DEFINE(input_ethernet, synapse_pb_Input);
-ZROS_TOPIC_DEFINE(input_sbus, synapse_pb_Input);
-ZROS_TOPIC_DEFINE(led_array, synapse_pb_LEDArray);
-ZROS_TOPIC_DEFINE(magnetic_field, synapse_pb_MagneticField);
-ZROS_TOPIC_DEFINE(moment_ff, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(moment_sp, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(nav_sat_fix, synapse_pb_NavSatFix);
-ZROS_TOPIC_DEFINE(odometry_estimator, synapse_pb_Odometry);
-ZROS_TOPIC_DEFINE(odometry_ethernet, synapse_pb_Odometry);
 ZROS_TOPIC_DEFINE(optical_flow_raw, synapse_pb_PixartPAA3905);
-ZROS_TOPIC_DEFINE(orientation_sp, synapse_pb_Quaternion);
-ZROS_TOPIC_DEFINE(position_sp, synapse_pb_Vector3);
-ZROS_TOPIC_DEFINE(pwm, synapse_pb_Pwm);
-ZROS_TOPIC_DEFINE(safety, synapse_pb_Safety);
 ZROS_TOPIC_DEFINE(status, synapse_pb_Status);
-ZROS_TOPIC_DEFINE(velocity_sp, synapse_pb_Vector3);
 ZROS_TOPIC_DEFINE(vehicle_optical_flow, synapse_topic_VehicleOpticalFlowData_t);
 ZROS_TOPIC_DEFINE(vehicle_optical_flow_vel, synapse_topic_VehicleOpticalFlowVelData_t);
-ZROS_TOPIC_DEFINE(wheel_odometry, synapse_pb_WheelOdometry);
 
 static struct zros_topic *topic_list[] = {
-	&topic_accel_ff,
-	&topic_accel_sp,
-	&topic_actuators,
-	&topic_altimeter,
-	&topic_angular_velocity_ff,
-	&topic_angular_velocity_sp,
 	&topic_argus,
-	&topic_attitude_sp,
-	&topic_battery_state,
-	&topic_bezier_trajectory,
-	&topic_bezier_trajectory_ethernet,
-	&topic_clock_offset_ethernet,
-	&topic_cmd_vel,
-	&topic_cmd_vel_ethernet,
 	&topic_imu0,
-	&topic_imu1,
-	&topic_imu2,
-	&topic_imu_q31_array,
-	&topic_input,
-	&topic_input_ethernet,
-	&topic_input_sbus,
-	&topic_led_array,
-	&topic_magnetic_field,
-	&topic_moment_ff,
-	&topic_nav_sat_fix,
-	&topic_odometry_estimator,
-	&topic_odometry_ethernet,
-	&topic_orientation_sp,
 	&topic_optical_flow_raw,
+	&topic_status,
 	&topic_vehicle_optical_flow,
 	&topic_vehicle_optical_flow_vel,
-	&topic_position_sp,
-	&topic_pwm,
-	&topic_safety,
-	&topic_status,
-	&topic_velocity_sp,
-	&topic_wheel_odometry,
 };
 
 static int set_topic_list()
